936a45eb03608947e22b2019b07018f84b51cb7f
Patrik Gebhardt <patrik.gebhardt@rub.de>
2014-03-19 12:29:34 +0100

    changing calibrator filter constant through dialog box added

--- calibratordialog.cpp
+++ calibratordialog.cpp
@@ -10,13 +10,15 @@ CalibratorDialog::CalibratorDialog(Calibrator* calibrator, QWidget *parent)
     // fill text boxes with calibrator data
     this->ui->regularization_factor_box->setText(QString("%1").arg(
         calibrator->eit_solver()->inverse_solver()->regularization_factor()));
-    this->ui->step_size_box->setText(QString("%1").arg(calibrator->step_size()));
+    this->ui->calibrator_interval_box->setText(QString::number((double)calibrator->step_size() * 1e-3, 'f', 1));
+    this->ui->filter_constant_box->setText(QString::number(calibrator->filterConstant(), 'f', 1));
 
     // update calibrator settings on acceptance
     connect(this, &QDialog::accepted, [=]() {
         calibrator->eit_solver()->inverse_solver()->regularization_factor() =
             this->ui->regularization_factor_box->text().toDouble();
-        calibrator->step_size() = this->ui->step_size_box->text().toInt();
+        calibrator->step_size() = (int)(this->ui->calibrator_interval_box->text().toDouble() * 1e3);
+        calibrator->filterConstant() = this->ui->filter_constant_box->text().toDouble();
     });
 }
 
